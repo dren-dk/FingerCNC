3e14f9d21c717e893ac55940e9065b648131b4be
Flemming Frandsen <dren.dk@gmail.com>
2017-12-27 13:55:55 +0100

    Went from 352 to 223 ms by removing sleep calls

--- fingerer/lcd.c
+++ fingerer/lcd.c
@@ -70,51 +70,45 @@ uint8_t u8x8_gpio_and_delay(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *ar
 uint8_t sw_spi(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr) {
   uint8_t takeover_edge = u8x8_GetSPIClockPhase(u8x8);
   uint8_t not_takeover_edge = 1 - takeover_edge;
- 
-  switch(msg)
-  {
-  case U8X8_MSG_BYTE_SEND: {
-      uint8_t* data = (uint8_t *)arg_ptr;
-      while( arg_int > 0 ) {
-	uint8_t b = *data;
-	data++;
-	arg_int--;
-	for (uint8_t i = 0; i < 8; i++ ) {
-	  if ( b & 128 )
-	    u8x8_gpio_SetSPIData(u8x8, 1);
-	  else
-	    u8x8_gpio_SetSPIData(u8x8, 0);
-	  b <<= 1;
-	  
-	  u8x8_gpio_SetSPIClock(u8x8, not_takeover_edge);
-	  u8x8_gpio_SetSPIClock(u8x8, takeover_edge);
-	}    
-      }
-      
-      break;
-  }
-      
-    case U8X8_MSG_BYTE_INIT:
+
+  if (msg == U8X8_MSG_BYTE_SEND) {
+    uint8_t* data = (uint8_t *)arg_ptr;
+    while( arg_int > 0 ) {
+      uint8_t b = *data;
+      data++;
+      arg_int--;
+      for (uint8_t i = 0; i < 8; i++ ) {
+	if ( b & 128 ) {
+	  u8x8_gpio_SetSPIData(u8x8, 1);
+	} else {
+	  u8x8_gpio_SetSPIData(u8x8, 0);
+	}
+	b <<= 1;
+	
+	u8x8_gpio_SetSPIClock(u8x8, not_takeover_edge);
+	u8x8_gpio_SetSPIClock(u8x8, takeover_edge);
+      }    
+    }
+
+  } else if (msg == U8X8_MSG_BYTE_INIT) {
       // disable chipselect 
       u8x8_gpio_SetCS(u8x8, u8x8->display_info->chip_disable_level);
-      // no wait required here
-      
-      // for SPI: setup correct level of the clock signal
+        // for SPI: setup correct level of the clock signal
       u8x8_gpio_SetSPIClock(u8x8, u8x8_GetSPIClockPhase(u8x8));
-      break;
-    case U8X8_MSG_BYTE_SET_DC:
-      u8x8_gpio_SetDC(u8x8, arg_int);
-      break;
-    case U8X8_MSG_BYTE_START_TRANSFER:
-      u8x8_gpio_SetCS(u8x8, u8x8->display_info->chip_enable_level);  
-      u8x8->gpio_and_delay_cb(u8x8, U8X8_MSG_DELAY_NANO, u8x8->display_info->post_chip_enable_wait_ns, NULL);
-      break;
-    case U8X8_MSG_BYTE_END_TRANSFER:
-      u8x8->gpio_and_delay_cb(u8x8, U8X8_MSG_DELAY_NANO, u8x8->display_info->pre_chip_disable_wait_ns, NULL);
-      u8x8_gpio_SetCS(u8x8, u8x8->display_info->chip_disable_level);
-      break;
-    default:
-      return 0;
+
+  } else if (msg == U8X8_MSG_BYTE_SET_DC) {
+    u8x8_gpio_SetDC(u8x8, arg_int);
+
+  } else if (msg == U8X8_MSG_BYTE_START_TRANSFER) {
+    u8x8_gpio_SetCS(u8x8, u8x8->display_info->chip_enable_level);  
+    u8x8->gpio_and_delay_cb(u8x8, U8X8_MSG_DELAY_NANO, u8x8->display_info->post_chip_enable_wait_ns, NULL);
+
+  } else if (msg == U8X8_MSG_BYTE_END_TRANSFER) {
+    u8x8->gpio_and_delay_cb(u8x8, U8X8_MSG_DELAY_NANO, u8x8->display_info->pre_chip_disable_wait_ns, NULL);
+    u8x8_gpio_SetCS(u8x8, u8x8->display_info->chip_disable_level);
+
+  } else {
+    return 0;
   }
   return 1;
 }
